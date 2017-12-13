7737b2e3b7764eb5027664d681f78ebcf7d1a002
madvirus <madvirus@madvirus.net>
2017-12-13 21:36:27 +0900

    add Assembler

---
+++ sp5-chap03/src/main/java/assembler/Assembler.java
@@ -0,0 +1,31 @@
+package assembler;
+
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+
+public class Assembler {
+
+	private MemberDao memberDao;
+	private MemberRegisterService regSvc;
+	private ChangePasswordService pwdSvc;
+
+	public Assembler() {
+		memberDao = new MemberDao();
+		regSvc = new MemberRegisterService(memberDao);
+		pwdSvc = new ChangePasswordService(memberDao);
+	}
+
+	public MemberDao getMemberDao() {
+		return memberDao;
+	}
+
+	public MemberRegisterService getMemberRegisterService() {
+		return regSvc;
+	}
+
+	public ChangePasswordService getChangePasswordService() {
+		return pwdSvc;
+	}
+
+}
