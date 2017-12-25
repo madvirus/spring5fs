2798c8e712074a01a502706b83ac8baa101d460c
madvirus <madvirus@madvirus.net>
2017-12-25 23:15:06 +0900

    임포트

---
+++ sp5-chap03/src/main/java/config/AppConfImport.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import spring.MemberDao;
+import spring.MemberPrinter;
+
+@Configuration
+@Import({AppConf2.class})
+public class AppConfImport {
+
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberPrinter memberPrinter() {
+		return new MemberPrinter();
+	}
+}
--- sp5-chap03/src/main/java/main/MainForConfigurationType.java
+++ sp5-chap03/src/main/java/main/MainForConfigurationType.java
@@ -3,15 +3,17 @@ package main;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import config.AppCtx;
+import config.AppConf1;
+import config.AppConf2;
 
 public class MainForConfigurationType {
 
 	public static void main(String[] args) throws Exception {
-		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+		AbstractApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(AppConf1.class, AppConf2.class);
 
-		AppCtx appCtx = ctx.getBean(AppCtx.class);
-		System.out.println(appCtx.getClass().getName());
+		AppConf1 appConf1 = ctx.getBean(AppConf1.class);
+		System.out.println(appConf1 != null);
 
 		ctx.close();
 	}
