75e832c96769fa918369af67942156afbf85d73d
madvirus <madvirus@madvirus.net>
2018-02-02 17:29:38 +0900

    ddl

---
+++ sp5-chap08/src/sql/ddl.sql
@@ -0,0 +1,14 @@
+create user 'spring5'@'localhost' identified by 'spring5';
+
+create database spring5fs character set=utf8;
+
+grant all privileges on spring4fs.* to 'spring5'@'localhost';
+
+create table spring5fs.MEMBER (
+    ID int auto_increment primary key,
+    EMAIL varchar(255),
+    PASSWORD varchar(100),
+    NAME varchar(100),
+    REGDATE datetime,
+    unique key (EMAIL) 
+) engine=InnoDB character set = utf8;
