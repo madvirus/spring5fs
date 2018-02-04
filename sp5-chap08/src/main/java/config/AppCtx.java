d11008794b6f5a50db531a9156d4daad2d621da0
madvirus <madvirus@madvirus.net>
2018-02-04 21:24:39 +0900

    준비.

---
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -0,0 +1,22 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		DataSource ds = new DataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+		ds.setUsername("spring5");
+		ds.setPassword("spring5");
+		ds.setInitialSize(2);
+		ds.setMaxActive(10);
+		ds.setMaxIdle(5);
+		return ds;
+	}
+}
