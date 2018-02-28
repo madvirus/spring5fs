908b8f06fb9934d82b37977e30d209056e25413d
madvirus <madvirus@madvirus.net>
2018-02-28 17:26:50 +0900

    validation

---
+++ sp5-chap12-bv/src/main/java/spring/RegisterRequest.java
@@ -0,0 +1,55 @@
+package spring;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class RegisterRequest {
+	@NotBlank
+	@Email
+	private String email;
+	@Size(min = 6)
+	private String password;
+	@NotEmpty
+	private String confirmPassword;
+	@NotEmpty
+	private String name;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isPasswordEqualToConfirmPassword() {
+		return password.equals(confirmPassword);
+	}
+}
---
+++ sp5-chap12-bv/src/main/webapp/WEB-INF/view/main.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>메인</title>
+</head>
+<body>
+    <p>환영합니다.</p>
+    <p><a href="<c:url value="/register/step1" />">[회원 가입하기]</a>
+</body>
+</html>
