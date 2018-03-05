b68c5988240dee932951c9dbc815440fb36ee32a
madvirus <madvirus@madvirus.net>
2018-03-05 22:31:13 +0900

    ing...

--- sp5-chap16/src/main/java/controller/RestMemberController.java
+++ sp5-chap16/src/main/java/controller/RestMemberController.java
@@ -1,33 +1,19 @@
 package controller;
 
-import java.io.IOException;
-import java.net.URI;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import spring.DuplicateMemberException;
 import spring.Member;
 import spring.MemberDao;
 import spring.MemberNotFoundException;
-import spring.MemberRegisterService;
-import spring.RegisterRequest;
 
 @RestController
 public class RestMemberController {
-	private MemberRegisterService registerService;
 	private MemberDao memberDao;
+	//private MemberRegisterService registerService;
 
 	@GetMapping("/api/members")
 	public List<Member> members() {
@@ -43,31 +29,31 @@ public class RestMemberController {
 		return member;
 	}
 
-	@PostMapping("/api/members")
-	public ResponseEntity<Object> newMember(@RequestBody @Valid RegisterRequest regReq) {
-		try {
-			Long newMemberId = registerService.regist(regReq);
-			URI uri = UriComponentsBuilder.fromPath("/api/members/{id}").buildAndExpand(newMemberId).toUri();
-			return ResponseEntity.created(uri).build();
-		} catch (DuplicateMemberException dupEx) {
-			return ResponseEntity.notFound().build();
-		}
-	}
-
-	@PostMapping("/api/members2")
-	public void newMember(@RequestBody @Valid RegisterRequest regReq, HttpServletResponse response) throws IOException {
-		try {
-			Long newMemberId = registerService.regist(regReq);
-			response.setHeader("Location", "/api/members/" + newMemberId);
-			response.setStatus(HttpServletResponse.SC_CREATED);
-		} catch (DuplicateMemberException dupEx) {
-			response.sendError(HttpStatus.NOT_FOUND.value());
-		}
-	}
-
-	public void setRegisterService(MemberRegisterService registerService) {
-		this.registerService = registerService;
-	}
+//	@PostMapping("/api/members")
+//	public ResponseEntity<Object> newMember(@RequestBody @Valid RegisterRequest regReq) {
+//		try {
+//			Long newMemberId = registerService.regist(regReq);
+//			URI uri = UriComponentsBuilder.fromPath("/api/members/{id}").buildAndExpand(newMemberId).toUri();
+//			return ResponseEntity.created(uri).build();
+//		} catch (DuplicateMemberException dupEx) {
+//			return ResponseEntity.notFound().build();
+//		}
+//	}
+//
+//	@PostMapping("/api/members2")
+//	public void newMember(@RequestBody @Valid RegisterRequest regReq, HttpServletResponse response) throws IOException {
+//		try {
+//			Long newMemberId = registerService.regist(regReq);
+//			response.setHeader("Location", "/api/members/" + newMemberId);
+//			response.setStatus(HttpServletResponse.SC_CREATED);
+//		} catch (DuplicateMemberException dupEx) {
+//			response.sendError(HttpStatus.NOT_FOUND.value());
+//		}
+//	}
+//
+//	public void setRegisterService(MemberRegisterService registerService) {
+//		this.registerService = registerService;
+//	}
 
 	public void setMemberDao(MemberDao memberDao) {
 		this.memberDao = memberDao;
