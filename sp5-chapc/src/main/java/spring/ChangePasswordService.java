8e4cf76209edef366d237d7e591b84653498d5dc
madvirus <madvirus@madvirus.net>
2018-03-11 22:29:58 +0900

    app c

---
+++ sp5-chapc/src/main/java/spring/ChangePasswordService.java
@@ -0,0 +1,24 @@
+package spring;
+
+import org.springframework.transaction.annotation.Transactional;
+
+public class ChangePasswordService {
+
+	private MemberDao memberDao;
+
+	@Transactional
+	public void changePassword(String email, String oldPwd, String newPwd) {
+		Member member = memberDao.selectByEmail(email);
+		if (member == null)
+			throw new MemberNotFoundException();
+
+		member.changePassword(oldPwd, newPwd);
+
+		memberDao.update(member);
+	}
+
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+}
---
+++ sp5-chapc/src/main/java/spring/MemberNotFoundException.java
@@ -0,0 +1,5 @@
+package spring;
+
+public class MemberNotFoundException extends RuntimeException {
+
+}
