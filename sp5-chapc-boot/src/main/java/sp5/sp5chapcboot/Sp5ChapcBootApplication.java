48bcd2877e1f3f5e8c93d8dedc76d54594b67aba
madvirus <madvirus@madvirus.net>
2018-03-13 12:19:10 +0900

    chapc boot

---
+++ sp5-chapc-boot/src/main/java/sp5/sp5chapcboot/Sp5ChapcBootApplication.java
@@ -0,0 +1,12 @@
+package sp5.sp5chapcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp5ChapcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp5ChapcBootApplication.class, args);
+	}
+}
